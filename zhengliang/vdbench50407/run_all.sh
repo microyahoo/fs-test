f8076f6136356dac289d3c5d4182942cea917ce0
Liang Zheng <zhengliang0901@gmail.com>
2026-08-13 23:35:23 +0800

    add more vdbench test cases
    Signed-off-by: Liang Zheng <zhengliang0901@gmail.com>

--- zhengliang/vdbench50407/run_all.sh
+++ zhengliang/vdbench50407/run_all.sh
@@ -1,74 +1,62 @@
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-write-4K-4K-64job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-read-4K-4K-64job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-read8-write2-4K-4K-64job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-write-4K-4K-128job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-read-4K-4K-128job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-read8-write2-4K-4K-128job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-write-4K-4K-256job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-read-4K-4K-256job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-read8-write2-4K-4K-256job
+#!/bin/bash
 
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-write-3G-4k-128job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-read-3G-4k-128job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-read8-write2-3G-4k-128job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-write-3G-4k-256job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-read-3G-4k-256job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-read8-write2-3G-4k-256job
+BASE_DIR=/root/zhengliang/vdbench50407
+HOSTS=${BASE_DIR}/hosts
+VDBENCH=${BASE_DIR}/vdbench
 
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-write-3G-1M-64job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-read-3G-1M-64job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-read8-write2-3G-1M-64job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-write-3G-1M-128job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-rand-read-3G-1M-128job
-sleep 1m
-ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/21client-read8-write2-3G-1M-128job
+drop_caches() {
+    ansible all -i "${HOSTS}" -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
+}
 
-#sleep 1m
-#ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-##ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a '/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/1client-rand-read-3G-4k-128job'
-#/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/1client-rand-read-3G-4k-128job
-#
-#sleep 1m
-#ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a "echo 3 > /proc/sys/vm/drop_caches"
-#/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/1client-rand-read-3G-4k-256job
-##ansible all -i /root/zhengliang/vdbench50407/hosts -m shell --forks 15 -a '/root/zhengliang/vdbench50407/vdbench -f /root/zhengliang/vdbench50407/1client-rand-read-3G-4k-256job'
+run_test() {
+    local test_name=$1
+    echo ">>> Running: ${test_name}"
+    drop_caches
+    ${VDBENCH} -f "${BASE_DIR}/${test_name}"
+    sleep 1m
+}
+
+# 4K文件 4K IO 64job
+run_test 21client-rand-write-4K-4K-64job
+run_test 21client-rand-read-4K-4K-64job
+run_test 21client-read8-write2-4K-4K-64job
+
+# 4K文件 4K IO 128job
+run_test 21client-rand-write-4K-4K-128job
+run_test 21client-rand-read-4K-4K-128job
+run_test 21client-read8-write2-4K-4K-128job
+
+# 4K文件 4K IO 256job
+run_test 21client-rand-write-4K-4K-256job
+run_test 21client-rand-read-4K-4K-256job
+run_test 21client-read8-write2-4K-4K-256job
+
+# 3G文件 4k IO 128job
+run_test 21client-rand-write-3G-4k-128job
+run_test 21client-rand-read-3G-4k-128job
+run_test 21client-read8-write2-3G-4k-128job
+
+# 3G文件 4k IO 256job
+run_test 21client-rand-write-3G-4k-256job
+run_test 21client-rand-read-3G-4k-256job
+run_test 21client-read8-write2-3G-4k-256job
+
+# 3G文件 1M IO 64job
+run_test 21client-rand-write-3G-1M-64job
+run_test 21client-rand-read-3G-1M-64job
+run_test 21client-read8-write2-3G-1M-64job
+
+# 3G文件 1M IO 128job
+run_test 21client-rand-write-3G-1M-128job
+run_test 21client-rand-read-3G-1M-128job
+run_test 21client-read8-write2-3G-1M-128job
+
+# 4K文件 4K IO depth=3 (含create/write/read/mix)
+run_test 21client-4K_4K_3deep
+run_test 21client-4K_4K_3deep_2x
+run_test 21client-4K_4K_3deep_16x
+
+# 4K文件 4K IO depth=10 (含create/write/read/mix)
+run_test 21client-4K_4K_10deep
+run_test 21client-4K_4K_10deep_2x
+run_test 21client-4K_4K_10deep_16x
