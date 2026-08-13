8e8c15eae84092227183b0028ea38c73ffba8f78
Liang Zheng <zhengliang0901@gmail.com>
2026-08-13 14:46:58 +0800

    add 10G_4k test cases
    Signed-off-by: Liang Zheng <zhengliang0901@gmail.com>

--- zhengliang/fio/replace.sh
+++ zhengliang/fio/replace.sh
@@ -1,2 +1,7 @@
 find /root/zhengliang/fio -type f -exec sed -i 's|/mnt/yrtest|/vepfsE-test|g' {} +
 #find /root/zhengliang/fio -type f -exec sed -i 's|vepfsE-test|vepfsE-test|g' {} +
+
+# for f in 3G_4K_*; do
+#     new="${f/3G/10G}"
+#     sed "s/3G/10G/g" "$f" > "$new"
+# done
